e57192f9a55c7cee81b59cf0d406ad13ec9acc62
AskaIvan <aska_gunawan_24rpl@student.smktelkom-mlg.sch.id>
2017-05-13 19:07:22 +0700

    Home-nya sudah... tinggal yang lain dulu

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl304/askmovie/HomeFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl304/askmovie/HomeFragment.java
@@ -3,9 +3,25 @@ package id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.adapter.MyAdapter;
+import id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.model.Results;
+import id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.model.ResultsRespons;
+import id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.service.GsonGetRequest;
+import id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.service.VolleySingleton;
 
 
 /**
@@ -13,17 +29,58 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
+    ArrayList<Results> mlist = new ArrayList<>();
+    MyAdapter myAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view);
+        rv.setHasFixedSize(true);
+        myAdapter = new MyAdapter(this, mlist, getContext());
+        rv.setAdapter(myAdapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        downloadDataResource();
+
+        return rootView;
     }
+
+    private void downloadDataResource() {
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=83e9bd45d01bdec860110180bf6d664b&language=en-US&page=1";
+
+        GsonGetRequest<ResultsRespons> myRequest = new GsonGetRequest<ResultsRespons>
+                (url, ResultsRespons.class, null, new Response.Listener<ResultsRespons>() {
+
+                    @Override
+                    public void onResponse(ResultsRespons response) {
+                        Log.d("FLOW", "onResponse: " + (new Gson().toJson(response)));
+                        fillColor(response.results);
+                        mlist.addAll(response.results);
+                        myAdapter.notifyDataSetChanged();
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("FLOW", "onErrorResponse: ", error);
+                    }
+                });
+        VolleySingleton.getInstance(this).addToRequestQueue(myRequest);
+    }
+
+    private void fillColor(List<Results> results) {
+        for (int i = 0; i < results.size(); i++)
+            results.get(i).color = ColorUtil.getRandomColor();
+    }
+
+
 
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl304/askmovie/service/VolleySingleton.java
@@ -0,0 +1,54 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.service;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl304.askmovie.HomeFragment;
+
+
+public class VolleySingleton {
+    private static volatile VolleySingleton mInstance;
+    private static HomeFragment mCtx;
+    private RequestQueue mRequestQueue;
+    
+    /*private VolleySingleton(Context context)
+    {
+        if (mInstance != null)
+        {
+            throw new RuntimeException(
+                    "Use getInstance() method to get the single instance of this class");
+        }
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }*/
+
+    private VolleySingleton(HomeFragment context) {
+        if (mInstance != null) {
+            throw new RuntimeException(
+                    "Use getInstance() method to get the single instance of this class");
+        }
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static VolleySingleton getInstance(HomeFragment context) {
+        if (mInstance == null) {
+            synchronized (VolleySingleton.class) {
+                if (mInstance == null) mInstance = new VolleySingleton(context);
+            }
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getActivity());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
